3f2b99797f857046116833508ecfc46ff1949e08
Mark <markliu2013@gmail.com>
2014-09-13 16:17:30 +0800

    drawSpectrum

--- public/javascripts/player.js
+++ public/javascripts/player.js
@@ -1,13 +1,12 @@
 function Player(loadedcallback, endedcallback) {
 	this.loadedcallback = loadedcallback;
 	this.endedcallback = endedcallback;
+	this.thread = null;
 }
 Player.prototype = {
 	_prepareAPI: function() {
-		//fix browser vender for AudioContext and requestAnimationFrame
+		//fix browser vender for AudioContext
 		window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext;
-		window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame;
-		window.cancelAnimationFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.mozCancelAnimationFrame || window.msCancelAnimationFrame;
 	},
 	init: function(opt) {
 		var thisPlayer = this;
@@ -37,11 +36,15 @@ Player.prototype = {
 			this.audio.src = url;
 		}
 		this.audio.play();
+		clearInterval(this.thread);
+		this.drawSpectrum(document.getElementById('spectrum-canvas'));
 	},
 	pause: function() {
+		clearInterval(this.thread);
 		this.audio.pause();
 	},
 	stop: function() {
+		clearInterval(this.thread);
 		this.audio.pause();
 		this.audio.currentTime = 0;
 	},
@@ -49,22 +52,35 @@ Player.prototype = {
 		var thisPlayer = this;
 		var ctx = canvas.getContext('2d');
 		var gradient = ctx.createLinearGradient(0, 0, 0, 200);
-		gradient.addColorStop(1, '#6dc3f9');
-		gradient.addColorStop(0.5, '#516fd7');
 		gradient.addColorStop(0, '#70cbfc');
-		ctx.fillStyle = gradient;
-
-		setInterval(function() {
+		gradient.addColorStop(0.3, '#516fd7');
+		gradient.addColorStop(1, '#6dc3f9');
+		var cWidth = canvas.width;
+		var cHeight = canvas.height;
+		var columnWidth = 10;
+		var columGap = 2;
+		var columnNum = cWidth / (columnWidth+columGap);
+		var step = Math.floor( thisPlayer.analyser.frequencyBinCount / columnNum);
+		var maxValue = 255;
+		var capPositionArray = [];
+		var capHeight = 2;
+		this.thread = setInterval(function() {
 			var array = new Uint8Array(thisPlayer.analyser.frequencyBinCount);
 			thisPlayer.analyser.getByteFrequencyData(array);
-			var marWidth = thisPlayer.columnWidth+thisPlayer.gapWidth;
-			var columnNum = canvas.width / marWidth;
-
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
 			for (var i=0; i<columnNum; i++) {
-				ctx.fillRect(i*marWidth,0,thisPlayer.columnWidth, Math.random()*200);
+				var value = array[i * step]/maxValue*cHeight;
+				ctx.fillStyle = '#4c5fd1';
+				if (value < capPositionArray[i]) {
+					ctx.fillRect(i*(columnWidth+columGap),canvas.height-(--capPositionArray[i]),columnWidth, capHeight);
+				} else {
+					ctx.fillRect(i*(columnWidth+columGap),canvas.height-value,columnWidth, capHeight);
+					capPositionArray[i] = value;
+				}
+				ctx.fillStyle = gradient;
+				ctx.fillRect(i*(columnWidth+columGap),canvas.height-value+capHeight,columnWidth, cHeight);
 			}
-		}, 50);
+		}, 20);
 	}
 }
 
